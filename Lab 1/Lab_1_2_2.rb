de7c24ff94f147b683699f2bbde87d528f3d76b9
Aviamore <96797655+Aviamore@users.noreply.github.com>
2023-03-28 16:28:48 +0300

    Update Lab_1_2_2.rb

--- Lab 1/Lab_1_2_2.rb
+++ Lab 1/Lab_1_2_2.rb
@@ -1,11 +1,13 @@
 #Найти сумму цифр числа, делящихся на 3
+
 def delitel(number)
 sum = 0
 number.to_s.chars.each do |digit|
   digit = digit.to_i
   sum += digit if digit % 3 == 0
+return sum
 end
 
 number = gets.to_i
 
-puts "Сумма цифр числа #{number}, делящихся на 3: #{sum}"
+puts "Сумма цифр числа #{delitel(sum)}"
