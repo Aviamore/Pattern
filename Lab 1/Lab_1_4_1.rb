422cd71e88276d58632a99cf389d4adb0f45ea94
Aviamore <96797655+Aviamore@users.noreply.github.com>
2023-04-02 13:54:18 +0300

    Add files via upload

---
+++ Lab 1/Lab_1_4_1.rb
@@ -0,0 +1,17 @@
+def find_two_smallest_indexes(arr)
+  smallest_index = arr.index(arr.min)
+  arr.delete_at(smallest_index)
+  second_smallest_index = arr.index(arr.min)
+  second_smallest_index += 1 if second_smallest_index >= smallest_index
+  [smallest_index, second_smallest_index]
+
+end
+
+n = gets.chomp.to_i
+arr = []
+n.times do |i|
+  puts "Введите #{i+1}-й элемент массива:"
+  arr << gets.chomp.to_i
+end
+puts find_two_smallest_indexes(arr)
+
