4948964c2d301c4dd519e663332c1c1972b87bae
Aviamore <96797655+Aviamore@users.noreply.github.com>
2023-04-02 22:07:32 +0300

    Add files via upload

---
+++ Lab 1/Lab_1_4_4.rb
@@ -0,0 +1,11 @@
+def alternating_integers_and_floats?(arr)
+  integers = arr.select { |n| n.is_a?(Integer) }
+  floats = arr.select { |n| n.is_a?(Float) }
+  alternating = integers.zip(floats).flatten.compact
+  alternating == arr
+end
+
+arr1 = [1, 2.5, 3, 4.5, 5]
+arr2 = [1, 2, 3, 4.5, 5]
+puts alternating_integers_and_floats?(arr1) # true
+puts alternating_integers_and_floats?(arr2) # false
