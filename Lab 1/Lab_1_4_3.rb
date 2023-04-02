50e25ace94c98a010d4e98c20d3bba73c4ae8ee2
Aviamore <96797655+Aviamore@users.noreply.github.com>
2023-04-02 19:38:47 +0300

    Add files via upload

---
+++ Lab 1/Lab_1_4_3.rb
@@ -0,0 +1,11 @@
+def count_local_maximals(arr)
+  arr.each_cons(3).count { |a, b, c| b > a && b > c }
+end
+
+n = gets.chomp.to_i
+arr = []
+n.times do |i|
+  puts "Введите #{i+1}-й элемент массива:"
+  arr << gets.chomp.to_i
+end
+puts count_local_maximals(arr) # [1, 2, 1, 5, 3, 2, 8] = 2
