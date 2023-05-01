aff32fbc08479021f8d133a452fcd47913cf1a73
Aviamore <96797655+Aviamore@users.noreply.github.com>
2023-05-01 21:14:08 +0300

    3.9

--- Lab 2/Lab_2_3/DataList.rb
+++ Lab 2/Lab_2_3/DataList.rb
@@ -1,26 +1,46 @@
 class DataList
   private_class_method :new
 
+  attr_writer :objects
+
   def initialize(objects)
     self.objects = objects
   end
-
+  
   def select_element(number)
     self.selected_num = number < objects.size ? number : nil
   end
-
+  
   def selected_id
     objects[selected_num].id
   end
-
+  
   def column_names
     []
   end
-
+  
   def data_table
-    protected
-
-  def table_fields(obj)
-    [obj.github, obj.contact, obj.last_name_and_initials]
+    result = []
+    counter = 0
+    objects.each do |obj|
+      row = []
+      row << counter
+      row.push(*table_fields(obj))
+      result << row
+      counter += 1
+    end
+    DataTable.new(result)
   end
+  
+  protected
+  
+  def table_fields(_obj)
+    []
+  end
+
+  private
+
+  attr_reader :objects
+  attr_accessor :selected_num
+  
 end
