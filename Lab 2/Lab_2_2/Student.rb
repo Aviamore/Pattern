88eead6c43b05ab195453b8d2614c85405a22c2e
Aviamore <96797655+Aviamore@users.noreply.github.com>
2023-05-01 14:45:27 +0300

    Update Student.rb

--- Lab 2/Lab_2_2/Student.rb
+++ Lab 2/Lab_2_2/Student.rb
@@ -30,46 +30,13 @@ class Student < StudentSuper
     @father_name = new_father_name
   end
   
-    def telegram=(new_telegram)
-    raise ArgumentError, "Invalid argument: telegram=#{new_telegram}" unless new_telegram.nil? || Student.valid_profile_name?(new_telegram)
-
-    @telegram = new_telegram
-  end
-
-  def git=(new_github)
-    raise ArgumentError, "Invalid argument: github=#{new_github}" unless new_github.nil? || Student.valid_profile_name?(new_github)
-
-    @git = new_git
-  end
-
-  def email=(new_email)
-    raise ArgumentError, "Invalid argument: email=#{new_email}" unless new_email.nil? || Student.valid_email?(new_email)
-
-    @email = new_email
-  end
-  
-  def phone=(new_phone)
-    raise ArgumentError, "Invalid argument: phone=#{new_phone}" unless new_phone.nil? || Student.valid_phone?(new_phone)
-
-    @phone = new_phone
-    
- def valid_contacts?
-    !phone.nil? || !telegram.nil? || !email.nil?
-  end
-
-  def valid_github?
-    !github.nil?
-  end
-    
-    def valid?
-    valid_contacts? && valid_github?
-  end
-    
   def set_contacts(contacts)
     self.phone = contacts[:phone] if contacts.key?(:phone)
     self.telegram = contacts[:telegram] if contacts.key?(:telegram)
     self.email = contacts[:email] if contacts.key?(:email)
-  end
+    
+  def last_name_and_initials
+    "#{last_name} #{first_name[0]}. #{father_name[0]}."
     
   def short_contact
     return "Tg: #{telegram}" unless telegram.nil?
@@ -79,15 +46,20 @@ class Student < StudentSuper
     nil
   end
 
-  def last_name_and_initials
-    "#{last_name} #{first_name[0]}. #{father_name[0]}."
-  end
-
   def get_info
-    "#{last_name_and_initials}, #{short_contact}, Git: #{github}"
+    info = {}
+    info[:last_name_and_initials] = last_name_and_initials
+    info[:contact] = short_contact
+    info[:github] = github
+    info
   end
    
   def to_s
-    "ID: #{@id}, Фамилия: #{@last_name}, Имя: #{@first_name}, Отчество: #{@father_name}, Телефон: #{@phone}, Телеграм: #{@telegram}, Почта: #{@email}, Гит: #{@github}"
+    result = "#{last_name} #{first_name} #{father_name}"
+    %i[id phone telegram email git].each do |attr|
+      attr_val = send(attr)
+      result += ", #{attr}=#{attr_val}" unless attr_val.nil?
+    end
+    result
   end
 end
