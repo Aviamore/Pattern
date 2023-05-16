e69480c1424aaeb75ce6d8e80295a791a6c8574f
Aviamore <96797655+Aviamore@users.noreply.github.com>
2023-05-17 01:38:44 +0300

    Update Students_list_YAML.rb

--- Lab 2/Lab_2_4/Students_list_YAML.rb
+++ Lab 2/Lab_2_4/Students_list_YAML.rb
@@ -1,12 +1,12 @@
 class Students_list_YAML < StudentsList
+  def initialize(file_path)
+    super(file_path, YAMLSerializationStrategy.new)
+  end
+
   def load_from_file
     return unless File.exist?(@file_path)
 
     file = File.read(@file_path)
     @students = YAML.load(file, [Symbol, StudentShort])
-  end
-
-  def serialize_students
-    YAML.dump(@students)
   end
 end
