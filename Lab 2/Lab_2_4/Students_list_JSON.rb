5273065df1e72bb64019d78c477b325639793791
Aviamore <96797655+Aviamore@users.noreply.github.com>
2023-05-17 01:38:26 +0300

    Update Students_list_JSON.rb

--- Lab 2/Lab_2_4/Students_list_JSON.rb
+++ Lab 2/Lab_2_4/Students_list_JSON.rb
@@ -1,12 +1,12 @@
 class Students_list_JSON < StudentsList
+  def initialize(file_path)
+    super(file_path, JSONSerializationStrategy.new)
+  end
+
   def load_from_file
     return unless File.exist?(@file_path)
 
     file = File.read(@file_path)
     @students = JSON.parse(file, object_class: StudentShort)
-  end
-
-  def serialize_students
-    JSON.generate(@students)
   end
 end
