52ddbfdfff747daa994780b6b3772cea2ba5edd9
Aviamore <96797655+Aviamore@users.noreply.github.com>
2023-05-23 21:26:09 +0300

    Update Students_list_JSON.rb

--- Lab 2/Lab_2_4/Students_list_JSON.rb
+++ Lab 2/Lab_2_4/Students_list_JSON.rb
@@ -1,14 +1,5 @@
-require 'json'
-
 class Students_list_JSON < StudentsList
-  def read_students
-    file = File.read(@filename)
-    @students = JSON.parse(file, object_class: Student)
-  end
-
-  def write_students
-    File.open(@filename, 'w') do |f|
-      f.write(JSON.generate(@students))
+  def initialize(filename)
+    super(filename, JSONFileStrategy.new)
     end
-  end
 end
