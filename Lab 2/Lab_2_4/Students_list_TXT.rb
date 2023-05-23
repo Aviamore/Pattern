8882ca4760123dde5ebc20e612edcd2b212b6d8d
Aviamore <96797655+Aviamore@users.noreply.github.com>
2023-05-23 21:26:22 +0300

    Update Students_list_TXT.rb

--- Lab 2/Lab_2_4/Students_list_TXT.rb
+++ Lab 2/Lab_2_4/Students_list_TXT.rb
@@ -1,13 +1,5 @@
 class Students_list_TXT < StudentsList
   def initialize(filename)
-    super(filename)
-  end
-
-  def read_students
-    super
-  end
-
-  def write_students
-    super
+    super(filename, TXTFileStrategy.new)
   end
 end
