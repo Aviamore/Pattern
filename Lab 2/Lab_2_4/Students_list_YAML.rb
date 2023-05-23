ecc6ecddd36a04337728d83c49e4d676290dab61
Aviamore <96797655+Aviamore@users.noreply.github.com>
2023-05-23 21:25:32 +0300

    Update Students_list_YAML.rb

--- Lab 2/Lab_2_4/Students_list_YAML.rb
+++ Lab 2/Lab_2_4/Students_list_YAML.rb
@@ -1,13 +1,5 @@
-require 'yaml'
-
 class Students_list_YAML < StudentsList
-  def read_students
-    @students = YAML.load_file(@filename)
-  end
-
-  def write_students
-    File.open(@filename, 'w') do |f|
-      f.write(YAML.dump(@students))
-    end
+  def initialize(filename)
+    super(filename, YAMLFileStrategy.new)
   end
 end
