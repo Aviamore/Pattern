b8aa560a6bf686388cbe3b0db88bf6335141c99d
Aviamore <96797655+Aviamore@users.noreply.github.com>
2023-05-23 15:59:39 +0300

    Update Students_list_YAML.rb

--- Lab 2/Lab_2_4/Students_list_YAML.rb
+++ Lab 2/Lab_2_4/Students_list_YAML.rb
@@ -1,12 +1,16 @@
-class Students_list_YAML < StudentsList
-  def initialize(file_path)
-    super(file_path, YAMLSerializationStrategy.new)
+require_relative 'super_students_list'
+require 'yaml'
+
+class StudentsListYAML < SuperStudentsList
+  public_class_method :new
+
+  protected
+
+  def str_to_hash_list(str)
+    YAML.safe_load(str).map { |h| h.transform_keys(&:to_sym) }
   end
 
-  def load_from_file
-    return unless File.exist?(@file_path)
-
-    file = File.read(@file_path)
-    @students = YAML.load(file, [Symbol, StudentShort])
+  def hash_list_to_str(hash_list)
+    hash_list.map { |h| h.transform_keys(&:to_s) }.to_yaml
   end
 end
