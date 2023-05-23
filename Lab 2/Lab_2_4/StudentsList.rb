9b379f7358d509a25436953246d23196fd8f1a7e
Aviamore <96797655+Aviamore@users.noreply.github.com>
2023-05-23 22:04:35 +0300

    Update StudentsList.rb

--- Lab 2/Lab_2_4/StudentsList.rb
+++ Lab 2/Lab_2_4/StudentsList.rb
@@ -1,66 +1,65 @@
 class StudentsList
   attr_accessor :students
 
-  def initialize(filename, strategy)
+  def initialize(filename)
+    @file_format_strategy = nil
     @filename = filename
-    @strategy = strategy
-    @students = []
+    set_value
   end
 
-   def read_students
-    file_data = @strategy.read(@filename)
-    @students = file_data.map do |student_data|
-      Student.new(student_data['id'], student_data['name'], student_data['surname'])
+  def write_to_file
+    @file_format_strategy.write(@filename, @students.map(&:to_h))
+  end
+
+  def get_k_n_student_short_list(list_number, quan_element)
+    message = "В текущем списке недостаточно элементов, чтобы получить #{list_number}-й список из #{quan_element}-ти элементов!"
+    raise(ArgumentError, message) unless valid_k_n?(list_number, quan_element)
+
+    student_short_arr = @students.map { |obj| StudentShort.from_object(obj) }
+    student_short_arr = student_short_arr[(list_number - 1) * quan_element...list_number * quan_element]
+    DataListStudentShort.new(student_short_arr)
+  end
+
+  def sort_by_full_name
+    @students.sort_by! { |obj| [obj.last_name, obj.first_name, obj.patronymic]}
+  end
+
+  def get_by_id(id)
+    @students.find { |obj| obj.id == id }
+  end
+
+  def append(object)
+    raise(ArgumentError,'Переданное значение должно быть типа Student!') unless valid_student?(object)
+    object.id = get_count_id
+    @students.push(object)
+  end
+
+  def replace_by_id(id, object)
+    raise(ArgumentError,'Переданное значение должно быть типа Student!') unless valid_student?(object)
+    index = @students.find_index { |obj|  obj.id == id}
+    object.id = @students[index].id
+    @students[index] = object
+  end
+
+  def delete_by_id(id)
+    index = @students.find_index { |obj| obj.id == id }
+    @students.delete_at(index) if index
+  end
+
+  def get_student_count
+    @students.length
+  end
+
+  private
+  
+  def set_value
+    file_loader = FileLoader.new
+    @file_format_strategy = file_loader.get_file_format_strategy(@filename)
+    @students = @file_format_strategy.read(@filename).map do |data|
+      if data['patronymic'].nil?
+        Student.new(data['id'], data['first_name'], data['last_name'])
+      else
+        Student.new(data['id'], data['first_name'], data['last_name'], data['patronymic'])
+      end
     end
   end
-
-  def write_students
-    file_data = @students.map do |student|
-      { 'id' => student.id, 'name' => student.name, 'surname' => student.surname }
-    end
-    @strategy.write(@filename, file_data)
-  end
-
-  def get_student_by_id(id)
-    @students.find { |student| student.id == id }
-  end
-
-  def get_k_n_student_short_list(k, n)
-    start_idx = (n - 1) * k
-    end_idx = start_idx + k
-    @students[start_idx...end_idx]
-  end
-
-  def sort_students_by_name
-    @students.sort_by! { |student| [student.surname, student.name] }
-  end
-
-  def add_student(name, surname)
-    new_id = @students.size + 1
-    new_student = Student.new(new_id, name, surname)
-    @students << new_student
-  end
-
-  def replace_student_by_id(id, name, surname)
-    student = get_student_by_id(id)
-    if student
-      student.name = name
-      student.surname = surname
-    else
-      raise "Student with id #{id} not found"
-    end
-  end
-
-  def remove_student_by_id(id)
-    student = get_student_by_id(id)
-    if student
-      @students.delete(student)
-    else
-      raise "Student with id #{id} not found"
-    end
-  end
-
-  def get_student_short_count
-    @students.size
-  end
-end
