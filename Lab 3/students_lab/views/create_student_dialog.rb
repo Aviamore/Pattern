e7a3b57c0aa9ebd1cb64879a71a6f43587e2eaeb
Aviamore <96797655+Aviamore@users.noreply.github.com>
2023-05-29 00:06:33 +0300

    Update create_student_dialog.rb

--- Lab 3/students_lab/views/create_student_dialog.rb
+++ Lab 3/students_lab/views/create_student_dialog.rb
@@ -3,34 +3,36 @@ require 'fox16'
 
 include Fox
 class CreateStudentDialog<FXDialogBox
-  def initialize(parent, controller, student:nil)
+  def initialize(parent, controller)
     # Создаем родительское модальное окно
-    super(parent, "Добавление студента", DECOR_TITLE | DECOR_BORDER | DECOR_RESIZE)
+    super(parent, "Студент", DECOR_TITLE | DECOR_BORDER | DECOR_RESIZE)
     @controller = controller
-    @student = student
-    # Устанавливаем размер окна и делаем его модальным
+    @student = nil
+
     setWidth(400)
     setHeight(300)
     add_fields
-    # setModal(true)
   end
 
+  def set_student(student)
+    @student=student
+    enter_student
+  end
+
+  private
   def add_fields
-
-
     frame_data = FXVerticalFrame.new(self, :opts=> LAYOUT_FILL_X|LAYOUT_FILL_Y )
 
+    #создание полей
     field_name =[[:last_name,'Фамилия'], [:first_name, 'Имя'], [:second_name, 'Отчество'], [:git, 'Гит'], [:email, 'Почта'], [:phone, 'Телефон'], [:telegram, 'Телеграм']]
-    field_text = {}
+    @field_text = {}
     field_name.each do |field|
       frame_field = FXHorizontalFrame.new(frame_data )
       field_label = FXLabel.new(frame_field, field[1], :opts => LAYOUT_FIX_WIDTH)
       field_label.setWidth(100)
       text = FXTextField.new(frame_field, 40, :opts=>TEXTFIELD_NORMAL)
-      field_text[field[0]] = text
+      @field_text[field[0]] = text
     end
-
-    #если student не nil, то заполнить поля и запретить изменения всего, кроме фио
 
     btn_frame = FXHorizontalFrame.new(frame_data, LAYOUT_CENTER_X)
     btn_add=FXButton.new(btn_frame, "Сохранить")
@@ -41,43 +43,50 @@ class CreateStudentDialog<FXDialogBox
     btn_back.textColor = Fox.FXRGB(0,23,175)
 
     btn_add.connect(SEL_COMMAND) do
+      @controller.save_student(@student)
       self.handle(btn_add, FXSEL(Fox::SEL_COMMAND,
                                   Fox::FXDialogBox::ID_ACCEPT), nil)
-      # @controller.on_add_click(@student)
     end
 
     btn_back.connect(SEL_COMMAND) do
       self.handle(btn_back, FXSEL(Fox::SEL_COMMAND,
                                    Fox::FXDialogBox::ID_CANCEL), nil)
-      # @controller.on_add_click(@student)
     end
 
     #валидация полей
-    field_text.each_key do |name_field|
-      field_text[name_field].connect(SEL_CHANGED) do |text_field|
+    @field_text.each_key do |name_field|
+      @field_text[name_field].connect(SEL_CHANGED) do |text_field|
         res = {}
-        field_text.each do |k,v|
+        @field_text.each do |k,v|
           text = v.text.empty? ? nil : v.text
           res[k] = text
         end
 
         result = @controller.validate_fields(res)
-        if result.class==Student
+        unless result.nil?
           @student = result
           btn_add.enable
         else
           btn_add.disable
         end
-
       end
     end
+  end
 
-
-    def execute
-      super
-      @student
+  def enter_student
+    unless @student.nil?
+      student_hash = @student.to_hash
+      @field_text.each_key do |name_field|
+        unless name_field==:first_name || name_field==:second_name || name_field==:last_name
+          @field_text[name_field].editable = false
+        end
+        @field_text[name_field].text = student_hash[name_field]
+      end
     end
   end
+
+
+
 end
 
 
